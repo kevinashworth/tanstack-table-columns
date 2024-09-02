b5559e273f29a138337c27c4f880727761e90920
Kevin Ashworth <kevinashworth@yahoo.com>
2024-09-02 09:57:48 -0700

    Initial commit
    Started from joaopaulomoraes/reactjs-vite-tailwindcss-boilerplate

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import PersonTable from './PersonTable'
+
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-blue-900">
+      <div className="max-w-3xl bg-white p-4">
+        <PersonTable />
+      </div>
+    </div>
+  )
+}
+
+export default App
